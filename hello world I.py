3ef932bff118f8f98a8c13e0ff87ef49f7db75ca
diane.ludin_pursuit <diane.ludin@pursuit.org>
2025-07-28 20:18:19 -0400

    Add files via upload
    Hello world start in python there are 4 more changed scripts as part of this project.

---
+++ hello world I.py
@@ -0,0 +1,11 @@
+# This is a simple Python script that prints "Hello, World!" to the console.
+
+def hello_world():
+  """
+  Prints the "Hello, World!" message.
+  """
+  print("Hello, World!")
+
+# Call the function to execute the script.
+if __name__ == "__main__":
+  hello_world()
